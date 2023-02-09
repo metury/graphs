87ae7ab704afa12dc429c74aba91e145443ae2ca
Tomáš Turek <tom.turek@atlas.cz>
2023-02-09 11:32:54 +0100

    update build scriptu

--- project/src/build.sh
+++ project/src/build.sh
@@ -6,13 +6,23 @@ printf " * Kompilace -> "
 javac cz/cuni/mff/java/graphs/*.java
 echo "Hotovo"
 
-echo " * Vytvoření dokumentace"
+printf " * Vytvoření dokumentace -> "
 mkdir -p docs/
-javadoc cz/cuni/mff/java/graphs/*.java -d docs
-echo "    Hotovo"
+javadoc cz/cuni/mff/java/graphs/*.java -d docs -quiet
+echo "Hotovo"
 
-echo " * Spuštění"
+echo "========"
+echo "Spuštění"
+echo "========"
+echo ""
 java cz.cuni.mff.java.graphs.Main
+echo ""
+echo "========"
+echo "Konec"
+echo "========"
 
-echo "Dot test"
-dot -Tsvg export.dot > graph.svg
+for var in $(ls *.dot); do
+	printf " * Dot test '$var'-> "
+	dot -Tsvg $var > $var.svg
+	echo "Hotovo '$var'.svg"
+done
